e00090bd0b166da3eec5b695f2d1b50d793dafc7
Gregory Bluvshteyn <greg.bluvshteyn@gmail.com>
2017-11-19 11:56:33 -0500

    * some refactoring

--- insertionSort/InsertionSort.ts
+++ insertionSort/InsertionSort.ts
@@ -1,17 +1,19 @@
-export const swap = ( arr , i , j ) => {
-	let temp = arr[ i ];
-	arr[ i ] = arr[ j ];
-	arr[ j ] = temp;
-	return arr;
-};
+export const insertionSort = ( arr: number[] ) => {
 
-export const InsertionSort = ( arr: number[] ) =>
-	arr.map(( val , i ) => {
+	const swap = ( arr , i , j ) => {
+		let temp = arr[ i ];
+		arr[ i ] = arr[ j ];
+		arr[ j ] = temp;
+		return arr;
+	};
+
+	return arr.map(( val , i ) => {
 		for( let j = i + 1; j > 0; j-- ) {
 			if( arr[ j ] < arr[ j - 1 ] ) {
 				swap(arr , j , j - 1);
 			} else {
-				break;
+				return;
 			}
 		}
 	});
+}
--- insertionSort/__tests__/InsertionSort.test.ts
+++ insertionSort/__tests__/InsertionSort.test.ts
@@ -1,12 +1,13 @@
-import {InsertionSort} from '../InsertionSort';
+import * as sorts from '../InsertionSort';
 
 let expect = require('expect.js');
 
-describe("[InsertionSort]", () => {
+describe("[insertionSort]", () => {
    it("should be able to sort ", () => {
       const arr = [1,3,2,4,30];
-      const result = InsertionSort(arr);
-      expect(arr).to.eql([1,2,3,4,30]);
 
+      const result = sorts.insertionSort(arr);
+
+      expect(arr).to.eql([1,2,3,4,30]);
    });
 });
